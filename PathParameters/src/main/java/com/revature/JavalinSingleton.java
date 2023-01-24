24d4290d2c978c5c8dec485143c8fae1705fd0c8
isabelsouza1 <isabel.rose.souza@gmail.com>
2023-01-24 00:34:07 +0000

    Saved progress on PathParameters 2023-01-24 00:34:07 +0000

--- PathParameters/src/main/java/com/revature/JavalinSingleton.java
+++ PathParameters/src/main/java/com/revature/JavalinSingleton.java
@@ -15,9 +15,8 @@ public class JavalinSingleton {
          * Note: Please refer to the "PathParameters.MD" file for more assistance if needed.
          */
         app.get("/firstname/{first}", ctx -> {
-            
-            //write code here
-
+            String firstName = ctx.pathParam("first");
+            ctx.result(firstName);
         });
 
 
