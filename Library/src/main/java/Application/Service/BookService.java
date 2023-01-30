1db3d358409e661adc59f22fa2c0c12fc499e60e
isabelsouza1 <isabel.rose.souza@gmail.com>
2023-01-30 04:42:53 +0000

    Saved progress on Library 2023-01-30 04:42:53 +0000

--- Library/src/main/java/Application/Service/BookService.java
+++ Library/src/main/java/Application/Service/BookService.java
@@ -40,7 +40,7 @@ public class BookService {
      * @return all books.
      */
     public List<Book> getAllBooks() {
-        bookDAO.getAllBooks();
+        return bookDAO.getAllBooks();
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
@@ -61,7 +61,7 @@ public class BookService {
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-       // return bookDAO.getBooksWithBookCountOverZero();
+        return bookDAO.getBooksWithBookCountOverZero();
     }
 
 }
